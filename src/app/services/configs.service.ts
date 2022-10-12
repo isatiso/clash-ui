e7a417e174437f954c4ede43789362ff4c8c1724
Plank <sieglive@gmail.com>
2022-10-12 11:14:49 +0800

    -

--- src/app/services/configs.service.ts
+++ src/app/services/configs.service.ts
@@ -15,14 +15,8 @@ export class ConfigsService {
     constructor() {
         this._chart_style = this.get_local('chart_style') ?? 0
         this._language = this.get_local('lang') ?? 'en'
-        this._language_name = this._figure_language_name(this._language)
         this._theme = this.get_local('theme') ?? 'dark'
         this._backend = this.get_local('backend')
-    }
-
-    private _language_name = ''
-    get language_name(): string {
-        return this._language_name
     }
 
     private _chart_style: LocalStorageType['chart_style'] = 0
@@ -43,7 +37,6 @@ export class ConfigsService {
     set language(value: LocalStorageType['lang']) {
         this.set_local('lang', value)
         this._language = value
-        this._language_name = this._figure_language_name(this._language)
     }
 
     private _theme: LocalStorageType['theme']
@@ -81,17 +74,5 @@ export class ConfigsService {
     set_local<K extends keyof LocalStorageType>(key: K, value: LocalStorageType[K]) {
         const new_key = 'isatiso.clash.material.' + key
         localStorage.setItem(new_key, JSON.stringify(value))
-    }
-
-    private _figure_language_name(lang: string) {
-        lang = lang || this._language
-        switch (lang) {
-            case 'zh':
-                return '中文'
-            case 'en':
-                return 'English'
-            default:
-                return 'Unknown'
-        }
     }
 }
